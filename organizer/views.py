4b0286a01484182b742570f1af259b8a058b5c40
racheltstevens <racheltstevens@gmail.com>
2016-07-17 14:55:12 -0400

    Implement tag list view as homepage.

--- organizer/views.py
+++ organizer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template import Context, loader
 
-# Create your views here.
+from django.http.response import HttpResponse
+from .models import Tag
+
+def homepage(request):
+  tag_list = Tag.objects.all()
+  template = loader.get_template('organizer/tag_list.html')
+  context = Context({ 'tag_list': tag_list })
+  output = template.render(context)
+  return HttpResponse(output)
