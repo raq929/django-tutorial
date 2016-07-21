d00730789ffe559f03e82f6759882824e75a6b32
racheltstevens <racheltstevens@gmail.com>
2016-07-21 13:52:45 -0400

    Implement tag detail view
    refactor homepage view.

--- organizer/views.py
+++ organizer/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import (
+  get_object_or_404, render)
 from django.template import Context, loader
 
 from django.http.response import HttpResponse
 from .models import Tag
 
 def homepage(request):
-  tag_list = Tag.objects.all()
-  template = loader.get_template('organizer/tag_list.html')
-  context = Context({ 'tag_list': tag_list })
-  output = template.render(context)
-  return HttpResponse(output)
+  return render(
+    request,
+    'organizer/tag_list.html',
+    {'tag_list': Tag.objects.all() })
+
+def tag_detail(request, slug):
+  tag = get_object_or_404(Tag, slug__iexact=slug)
+  return render(request,
+    'organizer/tag_detail.html',
+    { 'tag': tag })
