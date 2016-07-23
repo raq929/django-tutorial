6ddd68c48a1202152b2deb39721317aa9147c887
racheltstevens <racheltstevens@gmail.com>
2016-07-23 15:34:26 -0400

    Create a class based view for post list.

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import View
 
 from .models import Post
 
-def post_list(request):
-  return render(
-    request,
-    'blog/post_list.html',
-    { 'post_list': Post.objects.all() })
+class PostList(View):
 
-def post_detail(request, year, month, slug):
+  def get(self, request):
+    return render(
+      request,
+      'blog/post_list.html',
+      {'post_list': Post.objects.all() })
+
+def post_detail(request, year, month, slug, parent_template=None):
   post = get_object_or_404(
     Post,
     pub_date__year=year,
