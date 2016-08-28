e326a89c7ccfa03f18e5497059540d51aef74e6e
racheltstevens <racheltstevens@gmail.com>
2016-08-28 15:30:13 -0400

    Implement all forms.

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Tag
+from .models import NewsLink, Startup, Tag
 
-
-class TagForm(forms.ModelForm):
+class NewsLinkForm(forms.ModelForm):
   class Meta:
-    model = Tag
+    model = NewsLink
     fields = '__all__'
 
-  def clean_name(self):
-    return self.cleaned_data['name'].lower()
+
+class SlugCleanMixin:
+  """Mixin class for slug cleaning method."""
 
   def clean_slug(self):
     new_slug = (
@@ -19,3 +19,19 @@ class TagForm(forms.ModelForm):
       raise ValidationError(
         'Slug may not be "create".')
     return new_slug
+
+
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+  class Meta:
+    model = Startup
+    fields = '__all__'
+
+
+class TagForm(SlugCleanMixin, forms.ModelForm):
+  class Meta:
+    model = Tag
+    fields = '__all__'
+
+  def clean_name(self):
+    return self.cleaned_data['name'].lower()
+
